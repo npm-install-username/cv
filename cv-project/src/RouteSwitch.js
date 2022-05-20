41317b5d8d795d754486bd373d4922acd7a27a10
Mohammed <m.alsami@hotmail.com>
2022-05-20 19:03:12 +0100

    added basis for navbar component - need to integrate sass next

--- cv-project/src/RouteSwitch.js
+++ cv-project/src/RouteSwitch.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
 import CVBuilder from "./components/CVBuilder";
+import Navbar from "./components/Navbar";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/profile" element={<Profile />} />
--- cv-project/src/components/Navbar.js
+++ cv-project/src/components/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-background'>
+      <ul>
+        <li>CVBuilder</li>
+        <li>Profile</li>
+        <li>Build a</li>
+      </ul>
+    </div>
   )
 }
 
