94a57bd2ab0234b1de0edfecd5204bbb72d5f77d
Mohammed <m.alsami@hotmail.com>
2022-05-21 15:04:20 +0100

    added basic styling to personal summary section

--- cv-project/src/components/PersonalSummary.js
+++ cv-project/src/components/PersonalSummary.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import TextInput from './TextInput';
+import '../styles/CVBuilder.scss'
 
 function PersonalSummary() {
   return (
-    <div class="container">    
+    <div class="container">
+        <div className="section-heading">
+            <h2>Personal Summary</h2>  
+        </div>  
         <TextInput />
     </div>
   )
