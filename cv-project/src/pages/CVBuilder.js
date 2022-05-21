271c1c19ce619c6b02501595ac61197ee2408fb5
Mohammed <m.alsami@hotmail.com>
2022-05-21 14:46:33 +0100

    added tiny mce content editor for the text - need to add api key eventually

--- cv-project/src/pages/CVBuilder.js
+++ cv-project/src/pages/CVBuilder.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import TextInput from '../components/TextInput';
 
 function CVBuilder() {
+
   return (
-    <div>CVBuilder</div>
+    <TextInput />
   )
 }
 
