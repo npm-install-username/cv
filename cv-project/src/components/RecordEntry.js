489573f62408a53c0ac5c9f3549feb7ae54ebf88
Mohammed <m.alsami@hotmail.com>
2022-05-26 22:02:49 +0100

    added basic styling to record entry

--- cv-project/src/components/RecordEntry.js
+++ cv-project/src/components/RecordEntry.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import '../styles/RecordEntry.scss'
 import { FaTrash, FaPen } from 'react-icons/fa';
+
 
 function RecordEntry(props) {
     
 
     if (props.title==='Education') {
         return (
-            <div>
-                <h4>{props.entryContent.place}</h4>
+            <div className='record-entry-preview'>
+                <h4>{props.entryContent.place} <FaPen /> <FaTrash/> </h4>
                 <h5>{props.entryContent.startDate} - {props.entryContent.endDate}</h5>
                 <h5>{props.entryContent.degree}</h5>
             </div>
@@ -15,7 +17,7 @@ function RecordEntry(props) {
     } 
     if (props.title === 'Professional Experience'){
         return (
-            <div>
+            <div className='record-entry-preview'>
                 <h4>{props.entryContent.place}</h4>
                 <h5>{props.entryContent.startDate} - {props.entryContent.endDate}</h5>
                 <h5>{props.entryContent.jobTitle}</h5>
