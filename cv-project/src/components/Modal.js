d034f24f065dc70322b2dcfa34080fcc273d53cc
Mohammed <m.alsami@hotmail.com>
2022-05-27 11:29:22 +0100

    made modal flexible to different device sizes

--- cv-project/src/components/Modal.js
+++ cv-project/src/components/Modal.js
@@ -9,58 +9,58 @@ function Modal(props) {
   return (
     <>
         <div className="darkBG" onClick={() => props.setIsOpen(false)} />
-        <div className="centered">
-        <div className="modal">
-            <div className="modalHeader">
-            <h5 className="heading">Dialog</h5>
-            </div>
-            <button className="closeBtn" onClick={() => props.setIsOpen(false)}>
-            <RiCloseLine style={{ marginBottom: "-3px" }} />
-            </button>
-            <div className="modalContent">
-                <form >
-                    <label htmlFor="place">Company</label>
-                    <input type="text" title='place'/>
-                    {props.title === 'Education' &&
-                        <div className="education-info-Wrapper">
-                            <label htmlFor="degree">Degree</label>
-                            <input type="text" title='degree'/>
-                            <label htmlFor="Grade">Grade</label>
-                            <input type="text" title='endDate'/>
-                        </div>
-                    }
-                    {props.title === 'Professional Experience' &&
-                        <div className="work-info-Wrapper">
-                            <label htmlFor="jobTitle">Title</label>
-                            <input type="text" title='jobTitle'/>
+            <div className="centered">
+                <div className="modal">
+                    <div className="modalHeader">
+                    <h5 className="heading">Add Info</h5>
+                    </div>
+                    <button className="closeBtn" onClick={() => props.setIsOpen(false)}>
+                    <RiCloseLine style={{ marginBottom: "-3px" }} />
+                    </button>
+                    <div className="modalContent">
+                        <form >
+                            <label htmlFor="place">Company</label>
+                            <input type="text" title='place'/>
+                            {props.title === 'Education' &&
+                                <div className="education-info-Wrapper">
+                                    <label htmlFor="degree">Degree</label>
+                                    <input type="text" title='degree'/>
+                                    <label htmlFor="Grade">Grade</label>
+                                    <input type="text" title='endDate'/>
+                                </div>
+                            }
+                            {props.title === 'Professional Experience' &&
+                                <div className="work-info-Wrapper">
+                                    <label htmlFor="jobTitle">Title</label>
+                                    <input type="text" title='jobTitle'/>
 
-                        </div>
-                    }
-                    <div className="date-Wrapper">
-                        <label htmlFor="startDate">Start Date</label>
-                        <input type="text" title='startDate'/>
-                        <label htmlFor="endDate">End Date</label>
-                        <input type="text" title='endDate'/>
+                                </div>
+                            }
+                            <div className="date-Wrapper">
+                                <label htmlFor="startDate">Start Date</label>
+                                <input type="text" title='startDate'/>
+                                <label htmlFor="endDate">End Date</label>
+                                <input type="text" title='endDate'/>
+                            </div>
+                            <div className="text-field">
+                                <TextInput />
+                            </div>
+                        </form>
                     </div>
-                    <div className="text-field">
-                        <TextInput />
+                    <div className="modalActions">
+                    <div className="actionsContainer">
+                        <button className="deleteBtn" onClick={() => props.setIsOpen(false)}>
+                        Delete
+                        </button>
+                        <button
+                        className="cancelBtn"
+                        onClick={() => props.setIsOpen(false)}
+                        >
+                        Cancel
+                        </button>
                     </div>
-                </form>
+                </div>
             </div>
-            <div className="modalActions">
-            <div className="actionsContainer">
-                <button className="deleteBtn" onClick={() => props.setIsOpen(false)}>
-                Delete
-                </button>
-                <button
-                className="cancelBtn"
-                onClick={() => props.setIsOpen(false)}
-                >
-                Cancel
-                </button>
-            </div>
-            </div>
-        </div>
         </div>
     </>
     ); 
